475a5bddaf6a8c79b88a0e1a7030c2654b4095b1
Leo Hrabovetskyi <leontiy1992@gmail.com>
2015-12-23 00:28:17 +0200

    Many work last 2 days

--- js/main.js
+++ js/main.js
@@ -27,32 +27,74 @@
 
     $(document).ready(function() {
 
-        /* $('#fullpage').fullpage({
 
-         menu: true,
-         lockAnchors: false,
-         anchors:['firstPage', 'secondPage', '3Page', '4Page'],
-         navigation: true,
-         navigationPosition: 'right',
+        /* last experience calculation */
 
-         verticalCentered: true
-         });*/
+        var today = new Date();
+        var aug14 = new Date('August 1, 2014');
+        var oneDay=1000*60*60*24; //milliseconds in one day
+
+        var diff = today - aug14;
+
+        var days = Math.round(diff/oneDay);
+
+        var years = Math.floor(days/365);
+        var month = Math.floor((days%365)/30);
+
+        var yearsString ='',
+            monthString = '';
+
+        if (years !== 0) {
+            yearsString = (years === 1) ? years + ' year ' : years + ' years ';
+        }
+
+        if (month !== 0) {
+            monthString = (month === 1) ? month + ' month ' : month + ' months';
+        }
+
+        var diffString = '(' + yearsString + monthString + ')';
+
+        $('.difference.now').text(diffString);
+
+
+
+        $('#fullpage').fullpage({
+            //Navigation
+            menu: true,
+            //anchors:['hello', 'story', 'skills1', 'experience1', 'portfolio1', 'hireme1', 'contact1'],
+            lockAnchors: true,
+            navigation: true
+            //navigationTooltips: ['intro', 'overnight','team','Services','bmodel','work','profits','Contacts'],
+        });
+
 
 
         var timelineBlocks = $('.timeline-block'),
             offset = 0.8;
 
+
+
         //hide timeline blocks which are outside the viewport
-        hideBlocks(timelineBlocks, offset);
+       hideBlocks(timelineBlocks, offset);
 
         //on scolling, show/animate timeline blocks when enter the viewport
-        $(window).on('scroll', function() {
+
+         $('#experience').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+
+            if (visible) {
+                timelineBlocks.find('.timeline-img, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
+                $(this).unbind('inview');
+            }
+        });
+
+
+       /* $(window).on('scroll', function() {
             (!window.requestAnimationFrame) ? setTimeout(function() {
                 showBlocks(timelineBlocks, offset);
             }, 100): window.requestAnimationFrame(function() {
                 showBlocks(timelineBlocks, offset);
             });
-        });
+        });*/
 
         function hideBlocks(blocks, offset) {
             blocks.each(function() {
@@ -107,6 +149,168 @@
         });
 
 
+        /* placeholder animation */
+
+        $('.form-group input, .form-group textarea').each(function() {
+
+            $(this).on('focus', function() {
+                $(this).parent('.form-group').addClass('active');
+            });
+
+            $(this).on('blur', function() {
+                if ($(this).val().length == 0) {
+                    $(this).parent('.form-group').removeClass('active');
+                }
+            });
+
+            if ($(this).val() != '') $(this).parent('.form-group').addClass('active');
+
+        });
+
+
+
+
+        /* google map */
+
+
+        function initMap() {
+
+            // Specify features and elements to define styles.
+            var styleArray = [
+                {
+                    featureType: "all",
+                    stylers: [
+                        { saturation: -10 }
+                    ]
+                }/*,{
+                    featureType: "road.arterial",
+                    elementType: "geometry",
+                    stylers: [
+                        { hue: "#00ffee" },
+                        { saturation: 50 }
+                    ]
+                },*//*{
+                    featureType: "poi.business",
+                    elementType: "labels",
+                    stylers: [
+                        { visibility: "off" }
+                    ]
+                }*/
+            ];
+
+            // Create a map object and specify the DOM element for display.
+            var map = new google.maps.Map(document.getElementById('map-canvas'), {
+                center: {lat: 50.4497967, lng: 30.5237217},
+                scrollwheel: false,
+                // Apply the map style array to the map.
+                styles: styleArray,
+                zoom: 10
+            });
+
+            var image = 'img/doge1.png';
+            var dogeMarker = new google.maps.Marker({
+                position: {lat: 50.4497967, lng: 30.5237217},
+                map: map,
+                icon: image
+            });
+        }
+
+        initMap();
+
+
+
+
+        /* Head rotate section */
+
+        var blockW = 0,
+            blockH = 0,
+            blockOffsetTop = 0,
+            centerW = 0,
+            centerH = 0,
+            relX = 0,
+            relY = 0;
+
+        var zeroVector = [2, 1];
+
+        windowWH();
+
+        window.onresize = function(){
+            windowWH();
+        }
+
+
+        document.querySelectorAll("#hire")[0].onmousemove = function(event){
+
+            event = event || window.event; // IE-ism
+
+            relX = event.pageX - centerW;
+            relY = centerH - event.pageY;
+
+            sectorFound(relX, relY);
+
+        }
+
+        function windowWH() {
+
+            var block = document.querySelectorAll("#hire")[0];
+
+            blockW = block.offsetWidth;
+            blockH = block.offsetHeight;
+
+            centerW = blockW/2;
+            centerH = blockH/2;
+
+        }
+
+        function sectorFound(relX, relY) {
+
+            var zero = zeroVector;
+            var point = [relX, relY];
+
+            var side = (relY >0) ? 1 : -1;
+
+            if (
+                relY <= 85 &&
+                relY >= -210 &&
+                relX <= 150 &&
+                relX >= -150 ) side = 0;
+
+            var scalMultVect = zero[0] * point[0] + zero[1] * point[1];
+
+            var zeroABS = Math.sqrt(Math.pow(zero[0],2) + Math.pow(zero[1],2));
+            var pointABS = Math.sqrt(Math.pow(point[0],2) + Math.pow(point[1],2));
+
+            var cosAlpha = scalMultVect/(zeroABS * pointABS);
+
+            var alpha = Math.acos(cosAlpha);
+
+            var angle = alpha * (180/Math.PI);
+
+            var preSector = Math.ceil(angle/30);
+
+            //console.log(preSector);
+
+            switch (side) {
+                case -1:
+                    sector = preSector;
+                    break;
+
+                case 0:
+                    sector = 13;
+                    break;
+
+                case 1:
+                    sector = 13 - preSector;
+            }
+
+            document.querySelectorAll(".head-rotate")[0].style['top'] = -(sector-1) * 450 + 'px';
+
+
+
+
+        }
+
+        /* END Head rotate section */
 
 
 
@@ -145,10 +349,37 @@
         }
 
         function singleLetters($words) {
+
+            var longestWordLength = 0;
+
+            $words.each(function(){
+                var letters = $(this).text().split('');
+                longestWordLength = (letters.length > longestWordLength) ? letters.length : longestWordLength;
+            });
+
+
             $words.each(function(){
                 var word = $(this),
                     letters = word.text().split(''),
                     selected = word.hasClass('is-visible');
+
+                // Makings string equals
+
+                if (letters.length != longestWordLength) {
+
+                    var diff = longestWordLength - letters.length,
+                        loops = Math.floor(diff/2);
+
+                    for (var i = 0; i < loops; i++) {
+                        letters.unshift(' ');
+                        letters.push(' ');
+                    }
+
+                    if (diff%2 == 1) {
+                        letters.push(' ');
+                    }
+                }
+
                 for (i in letters) {
                     if(word.parents('.rotate').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
                     letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
@@ -276,3 +507,5 @@
 
 
 })(jQuery);
+
+
